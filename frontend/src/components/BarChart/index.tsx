749e043a3c1fd55de220ae177bd79aa80b8a4b86
erickos-dev <62565591+erickos-dev@users.noreply.github.com>
2021-09-19 21:18:55 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,61 @@
+import axios from "axios";
+import { type } from "os";
+import { useEffect, useState } from "react";
+
 import ReactApexChart from "react-apexcharts";
+import { SaleSucces } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+
+
 
 const BarChart = () => {
+    type SeriesData = {
+        name: string;
+        data: number[];
+    }
+    
+    type ChartData = {
+        labels: {
+            categories: string[];
+        };
+        series: SeriesData[];
+    }
+    
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    })
+    
+    
+useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSucces[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
 
+            setChartData({ labels: {
+                categories: myLabels
+            },
+            series: [
+                {
+                    name: "% Success",
+                    data: mySeries
+                }
+            ]});
+            //console.log("🚀 ~ file: index.tsx ~ line 24 ~ DonutChart ~ chartData", chartData)
+
+        })
+}, [])
     const options = {
         plotOptions: {
             bar: {
@@ -9,21 +63,9 @@ const BarChart = () => {
             }
         },
     };
-
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-
+  
     return (
-        <ReactApexChart options={{ ...options, xaxis: mockData.labels }} series={mockData.series} type="bar" height={350} />
+        <ReactApexChart options={{ ...options, xaxis: chartData.labels }} series={chartData.series} type="bar" height={350} />
     );
 }
 
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,10 @@
-export type SaleSum =
-    {
-        sellerName: string;
-        sum: number;
-    }
+export type SaleSum = {
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSucces = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
